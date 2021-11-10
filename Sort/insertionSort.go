173e1765cc2e9073dd1d4130f83e7f8ab9048280
Juan Fletes <juanvfletes@gmail.com>
2021-11-09 21:45:40 -0600

    Added Insertion Sort data

--- Sort/insertionSort.go
+++ Sort/insertionSort.go
@@ -1,6 +1,9 @@
 package Sort
 
-import "fmt"
+import (
+	"strconv"
+	"time"
+)
 
 type InsertionSort struct {
 	Array *[]int
@@ -16,36 +19,51 @@ func (in *InsertionSort) GetArray() *[]int {
 	return in.Array
 }
 
+func (in *InsertionSort) Sort() (Item, bool) {
+	itm, more := <-in.ch
+	return itm, more
+}
+
 func (in *InsertionSort) sort() {
 	i := 0
 	key := 0
 	j := 0
 
+	t0 := time.Now().UnixNano() / int64(time.Millisecond)
+	comparisons := 0
+	swaps := 0
+	iterations := 1
+
 	for i = 0; i < len(*in.Array); i++ {
-		//temp_i := i
+		iterations++
 		key = (*in.Array)[i]
 		j = i - 1
 
 		for {
 			if j < 0 || (*in.Array)[j] <= key {
+				comparisons++
 				break
 			}
 
+			swaps++
 			in.ch <- Item{IndexFrom: j, IndexTo: j + 1}
 			(*in.Array)[j+1] = (*in.Array)[j]
 			j = j - 1
 		}
 
-	//	in.ch <- Item{IndexFrom: temp_i, IndexTo: j + 1}
 		(*in.Array)[j+1] = key
+	}
+	t1 := time.Now().UnixNano() / int64(time.Millisecond)
+
+	in.ch <- Item{
+		Finished:   true,
+		TimeEnd:    strconv.FormatInt(t0, 10),
+		TimeStart:  strconv.FormatInt(t1, 10),
+		TotalTime:  strconv.FormatInt(t1-t0, 10),
+		TotalComp:  comparisons,
+		TotalSwaps: swaps,
+		TotalIter:  iterations,
 	}
 
 	close(in.ch)
-
-	fmt.Println(*in.Array)
-}
-
-func (in *InsertionSort) Sort() (Item, bool) {
-	itm, more := <-in.ch
-	return itm, more
 }
